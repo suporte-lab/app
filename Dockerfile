eaa39076073570dcc9667c2f3abe81617c4acde4
Andre Fernandes <andre@Andres-MacBook-Air.local>
2025-10-28 15:11:14 +0000

    Add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM oven/bun:1
+
+WORKDIR /app
+
+COPY . .
+
+RUN bun install --frozen-lockfile || bun install
+
+RUN cd frontend && bun install --frozen-lockfile || bun install && bun run build
+
+EXPOSE 3000
+
+CMD ["bun",  "start"]
--- server/app.ts
+++ server/app.ts
@@ -14,6 +14,6 @@ app.get("/test", (c) => {
 app.route("/api/expenses", expensesRoute);
 
 app.get("*", serveStatic({ root: "./frontend/dist" }));
-app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
+app.get("*", serveStatic({ path: "./frontend/dist" }));
 
 export default app;
