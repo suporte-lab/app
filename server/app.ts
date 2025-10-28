947854c2151032e77238b7e4bf471f0e1a28366d
Andre Fernandes <andre@Andres-MacBook-Air.local>
2025-10-28 15:51:18 +0000

    add react query, router

--- server/app.ts
+++ server/app.ts
@@ -1,19 +1,16 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-import { expensesRoute } from "./routes/expenses";
 import { serveStatic } from "hono/bun";
+import { expensesRoute } from "./routes/expenses";
 
 const app = new Hono();
 
 app.use("*", logger());
 
-app.get("/test", (c) => {
-  return c.json({ message: "Test" });
-});
-
-app.route("/api/expenses", expensesRoute);
+const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute);
 
 app.get("*", serveStatic({ root: "./frontend/dist" }));
 app.get("*", serveStatic({ path: "./frontend/dist" }));
 
 export default app;
+export type ApiRoutes = typeof apiRoutes;
