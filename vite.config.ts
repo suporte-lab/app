0f3fcb46a9e2dcfb1f44ec038e7bbdc073b70148
Andre Fernandes <andre@Andres-MacBook-Air.local>
2025-10-28 12:04:43 +0000

    push

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,6 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    allowedHosts: true,
-  },
   plugins: [
     tsConfigPaths(),
     tanstackStart({ target: "bun", spa: { enabled: true } }),
