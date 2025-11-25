bb606c59e5b666c2c90d945afff9ed00993b9445
Andre Fernandes <andre@Andres-MacBook-Air.local>
2025-11-25 12:23:25 +0000

    improve research import dialog

--- frontend/src/components/research-import-dialog.tsx
+++ frontend/src/components/research-import-dialog.tsx
@@ -12,6 +12,7 @@ import { useState } from "react";
 import { toast } from "sonner";
 import { cn } from "@/lib/utils";
 import { fetchProjectsOptions, fetchResearchOptions } from "@/lib/api";
+import { ScrollArea } from "./ui/scroll-area";
 
 export function ResearchImportDialog({
   researchId,
@@ -79,80 +80,83 @@ export function ResearchImportDialog({
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent>
-        <div className="space-y-4">
-          <DialogHeader>
-            <DialogTitle>Importar dados</DialogTitle>
-            <DialogDescription>
-              Use o ficheiro para importar abaixo.
-            </DialogDescription>
-          </DialogHeader>
-          <div>
-            <form>
-              <label htmlFor="file-input" className="sr-only">
-                Choose file
-              </label>
-              <input
-                type="file"
-                name="file-input"
-                id="file-input"
-                className="block w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
+        <ScrollArea className="max-h-[80vh]">
+          <div className="space-y-4">
+            <DialogHeader>
+              <DialogTitle>Importar dados</DialogTitle>
+              <DialogDescription>
+                Use o ficheiro para importar abaixo.
+              </DialogDescription>
+            </DialogHeader>
+            <div>
+              <form>
+                <label htmlFor="file-input" className="sr-only">
+                  Choose file
+                </label>
+                <input
+                  type="file"
+                  name="file-input"
+                  id="file-input"
+                  className="block w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
     file:bg-gray-50 file:border-0
     file:me-4
     file:py-3 file:px-4
    "
-                onChange={(e) => {
-                  if (!e.target.files) {
-                    setImportFile(null);
-                    return;
-                  }
+                  onChange={(e) => {
+                    if (!e.target.files) {
+                      setImportFile(null);
+                      return;
+                    }
 
-                  const file = e.target.files[0];
+                    const file = e.target.files[0];
 
-                  if (file.type != "text/csv") {
-                    setImportFile(null);
-                    toast.error("Ficheiro invalido");
-                    return;
-                  }
+                    if (file.type != "text/csv") {
+                      setImportFile(null);
+                      toast.error("Ficheiro invalido");
+                      return;
+                    }
 
-                  setImportFile(e.target.files[0]);
-                  console.log(e.target.files);
-                }}
-                accept=".csv,text/csv"
-                multiple={false}
-              />
-            </form>
-          </div>
-          <div className="flex gap-2">
-            <Button
-              disabled={!importFile || isLoading}
-              onClick={() => importData()}
-            >
-              Importar
-            </Button>
-            <Button
-              variant="outline"
-              onClick={() => {
-                generateImportCSV();
-              }}
-            >
-              Gerar ficheiro tabela
-            </Button>
-          </div>
-          <div className="space-y-2">
-            {log.map((item) => (
-              <div
-                className={cn(
-                  "px-3 font-medium py-2 rounded-md text-foreground bg-foreground/10 text-xs",
-                  item.type == "error" && "text-destructive bg-destructive/10",
-                  item.type == "warning" && "text-amber-700 bg-amber-500/15",
-                  item.type == "success" && "text-green-700 bg-green-500/15"
-                )}
+                    setImportFile(e.target.files[0]);
+                    console.log(e.target.files);
+                  }}
+                  accept=".csv,text/csv"
+                  multiple={false}
+                />
+              </form>
+            </div>
+            <div className="flex gap-2">
+              <Button
+                disabled={!importFile || isLoading}
+                onClick={() => importData()}
               >
-                {item.message}
-              </div>
-            ))}
+                Importar
+              </Button>
+              <Button
+                variant="outline"
+                onClick={() => {
+                  generateImportCSV();
+                }}
+              >
+                Gerar ficheiro tabela
+              </Button>
+            </div>
+            <div className="space-y-2">
+              {log.map((item) => (
+                <div
+                  className={cn(
+                    "px-3 font-medium py-2 rounded-md text-foreground bg-foreground/10 text-xs",
+                    item.type == "error" &&
+                      "text-destructive bg-destructive/10",
+                    item.type == "warning" && "text-amber-700 bg-amber-500/15",
+                    item.type == "success" && "text-green-700 bg-green-500/15"
+                  )}
+                >
+                  {item.message}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   );
